5cc9827458cdd59bb02e396d922a7e1f8e30ec56
Ethereal2049 <113393791+Ethereal2049@users.noreply.github.com>
2023-02-15 18:57:05 -0500

    Add files via upload
    a very simple - + * / calculator

---
+++ cal.py
@@ -0,0 +1,30 @@
+import math
+
+def main():
+    
+    run_agian = True
+    
+    while(run_agian == True):
+        print("This is a calculater that calculates + - * / \n")
+
+        num1 = float(input("enter first number: "))
+        op = input("Enter operater: ")
+        num2 = float(input("enter second number: "))
+        if (op == "+"):
+            print(num1 + num2)
+        elif(op == "-"):
+            print(num1 - num2)
+        elif(op == "*"):
+            print(num1 * num2)
+        elif(op == "/"):
+            print(num1 / num2)
+        else:
+            print("Enter only + - * / \n")
+        answer = input("\n Do you want to do another calculation? \t Enter Yes if you want to do another calculation:\n\n")
+        if (answer != "yes" and answer != "Yes"):
+            run_agian = False
+    print("Bye")
+
+main()
+
+
